0deacecea18b3557df5c2bec43a7641763d0f144
arpithappu <53164718+arpithappu@users.noreply.github.com>
2019-07-25 13:57:23 +0530

    Delete c.py

--- c.py
+++
@@ -1,9 +0,0 @@
-data="Rajesh,Krish,manoj,suresh,Anu,Anish,Anush,kavya"
-names=data.upper().split(",")
-print(names)
-list=[]
-for name in names:
-    if name.startswith("A") or name.endswith("H"):
-        list.append(name)
-list.sort()
-print(list)

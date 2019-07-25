30a1805dde32151ad71162c136ce4ce5409bdd19
arpithappu <53164718+arpithappu@users.noreply.github.com>
2019-07-25 13:59:46 +0530

    Delete h.py

--- h.py
+++
@@ -1,8 +0,0 @@
-names=["pkm","aln","pvr","pkm","aln","gln","ivr","pvr","km","vp","cs","mcs"]
-c_names={}
-for name in names:
-    if c_names.get(name)==None:
-        c_names[name]=1
-    else:
-        c_names[name]+=1
-print(c_names)
